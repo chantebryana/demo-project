92a27c9e1e9f80c5c707ccc9b3caee7bac16f7fc
chantebryana <chauncybryana@gmail.com>
2016-02-02 22:34:22 +0000

    chart.js data manually matches mysql db
    manually input monthly sum data from mysql db violinTable to see what the chart looks like. Discovered "null" value as placeholder for months without any data. Used this site for tips on mysql db queries: http://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html and http://stackoverflow.com/questions/937652/mysql-select-sum-group-by-date

--- php/charts/chartA.php
+++ php/charts/chartA.php
@@ -4,27 +4,27 @@
         <script src="../js/Chart.js"></script>
         <script>
             var data = {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
                 datasets: [
                     {
-                        label: "My First dataset",
+                        label: "2014: Minutes Practiced per Month",
                         fillColor: "rgba(220,220,220,0.2)",
                         strokeColor: "rgba(220,220,220,1)",
                         pointColor: "rgba(220,220,220,1)",
                         pointStrokeColor: "#fff",
                         pointHighlightFill: "#fff",
                         pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [65, 59, 80, 81, 56, 55, 40]
+                        data: [null, null, null, null, null, null, null, null, null, null, 15, 135]
                     },
                     {
-                        label: "My Second dataset",
+                        label: "2015: Minutes Practiced per Month",
                         fillColor: "rgba(151,187,205,0.2)",
                         strokeColor: "rgba(151,187,205,1)",
                         pointColor: "rgba(151,187,205,1)",
                         pointStrokeColor: "#fff",
                         pointHighlightFill: "#fff",
                         pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [28, 48, 40, 19, 86, 27, 90]
+                        data: [315, 372, 278, 991, 803, 215, 376, 143, 225, 407, 175, null]
                     }
                 ]
             };
@@ -33,7 +33,7 @@
             function page_startup(){
                 /*global Chart*/
                 var ctx = document.getElementById("myChart").getContext("2d");
-                var myNewChart = new Chart(ctx).Line(data, {bezierCurve: false});
+                var myNewChart = new Chart(ctx).Line(data, {bezierCurve: true});
                 document.getElementById("placeLegendHere").innerHTML = myNewChart.generateLegend();
             }
         </script>
