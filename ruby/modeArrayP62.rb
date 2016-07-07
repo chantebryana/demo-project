377a455f627819084b7140bdc3a72c03b6799830
chantebryana <chauncybryana@gmail.com>
2016-07-07 22:31:54 +0000

    close but no cigar
    end result for Mode is messed up: element needs to be greater than some filler value, and then mode changes based on the filler value

--- ruby/modeArrayP62.rb
+++ ruby/modeArrayP62.rb
@@ -1,6 +1,7 @@
 dataArray = []
-limit = 10
+limit = 5
 47.times {dataArray << rand(1..limit)}
+#dataArray = [1, 2, 3, 4, 5, 3, 3, 3, 3, 4, 4]
 dataArray.sort!
 print "dataArray: " + dataArray.join(", ") + "\n"
 
@@ -13,6 +14,14 @@ dataArray.each do |element|
     end 
 end
 
-print "tallyArray elements: "
-tallyArray.each do |i| print i.to_s + " " end 
-print "\n"
+print "tallyArray printout: \n"
+tallyArray.each_with_index do |e, i| 
+    print "index " + i.to_s + ", element " + e.to_s + "\n"
+end 
+
+tallyArray.each_with_index do |element, index|
+    if element > mode
+        mode = index + 1
+    end 
+end
+print "mode equals the number " + mode.to_s + "\n"
