984b3ed6707b4de6d8544432f878cfc27d13ae4a
chantebryana <chauncybryana@gmail.com>
2016-07-13 19:46:19 +0000

    Edit original file and create new one
    original file: update function and create user-interface lines. New file: begin steps to separate files.

--- ruby/modeArrayP62.rb
+++ ruby/modeArrayP62.rb
@@ -21,4 +21,10 @@ def modeTrials (range, reps)
     return mode
 end
 
-puts "Here's your mode: " + modeTrials(10, 47).to_s + "\n"
+print "TOP LIMIT OF RANDOM NUMBER GENERATOR\nUser, input an integer then press ENTER: "
+range = gets.chomp.to_i
+
+print "DEFINE HOW OFTEN TO REPEAT\nUser, input an integer then press ENTER: "
+reps = gets.chomp.to_i
+
+puts "Here's your mode: " + modeTrials(range, reps).to_s + "\n"
