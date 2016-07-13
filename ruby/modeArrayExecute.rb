9ef39333c96747809925d05966ec0799cfe8c28e
chantebryana <chauncybryana@gmail.com>
2016-07-13 20:01:41 +0000

    Finish creating two separate files.  I win!

--- ruby/modeArrayExecute.rb
+++ ruby/modeArrayExecute.rb
@@ -1,1 +1,9 @@
-word
+require "./modeArrayP62.rb"
+
+print "TOP LIMIT OF RANDOM NUMBER GENERATOR\nUser, input an integer then press ENTER: "
+range = gets.chomp.to_i
+
+print "DEFINE HOW OFTEN TO REPEAT\nUser, input an integer then press ENTER: "
+reps = gets.chomp.to_i
+
+puts "Here's your mode: " + modeTrials(range, reps).to_s + "\n"
--- ruby/modeArrayP62.rb
+++ ruby/modeArrayP62.rb
@@ -21,10 +21,3 @@ def modeTrials (range, reps)
     return mode
 end
 
-print "TOP LIMIT OF RANDOM NUMBER GENERATOR\nUser, input an integer then press ENTER: "
-range = gets.chomp.to_i
-
-print "DEFINE HOW OFTEN TO REPEAT\nUser, input an integer then press ENTER: "
-reps = gets.chomp.to_i
-
-puts "Here's your mode: " + modeTrials(range, reps).to_s + "\n"
