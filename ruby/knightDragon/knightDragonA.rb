d8f828cc9a8db6b96bd509ea86388b4e4de537be
chantebryana <chauncybryana@gmail.com>
2016-08-09 22:58:05 +0000

    Create very simple function to if / else through user choices

--- ruby/knightDragon/knightDragonA.rb
+++ ruby/knightDragon/knightDragonA.rb
@@ -4,6 +4,14 @@ knightHP = 20
 print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\': "
 userChoice = gets.upcase.chomp 
 
-print userChoice + "\n"
+def choiceCheck (userChoice)
+    if userChoice == "FIGHT" 
+        print "Initiate fighting!\n"
+    elsif userChoice == "RUN"
+        print "Initiate running!!\n"
+    else 
+        print "Your selection doesn't match any available options. Please try again.\n"
+    end
+end 
 
-#userChoice functon
+choiceCheck(userChoice) 
