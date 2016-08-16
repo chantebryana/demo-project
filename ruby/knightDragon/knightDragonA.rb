0f14a27ef3c7e067b717dfcfeebe485cdebf39f9
chantebryana <chauncybryana@gmail.com>
2016-08-16 18:44:23 +0000

    Putzing around with reusing user prompt lines.
     This attempt at function didn't work, and doesn't seem efficient. Perhaps I'll just try looping through the project somehow.

--- ruby/knightDragon/knightDragonA.rb
+++ ruby/knightDragon/knightDragonA.rb
@@ -1,17 +1,25 @@
 dragonHP = 50
 knightHP = 20
 
+def userPrompt (knightHP)
+    print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\', then press ENTER: "
+    userChoice = gets.upcase.chomp 
+    return userChoice 
+end
+
 def choiceCheck (userChoice)
     if userChoice == "FIGHT" 
         print "Initiate fighting!\n"
     elsif userChoice == "RUN"
-        print "Initiate running!!\n"
+        print "Knight successfully runs away.\n"
+        
     else 
         print "Your selection doesn't match any available options. Please try again.\n"
     end
 end 
 
-print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\': "
-userChoice = gets.upcase.chomp 
+#print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\', then press ENTER: "
+#userChoice = gets.upcase.chomp 
 
+userPrompt(knightHP) 
 choiceCheck(userChoice) 
