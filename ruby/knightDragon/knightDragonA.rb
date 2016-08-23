87b6fb18c38713c172e12b8a62d144de08ef93be
chantebryana <chauncybryana@gmail.com>
2016-08-23 19:58:49 +0000

    Rudimentary progression with single round of game
    add some dice rolls and spit out results.  Lots more I could do within this single round to make things better.

--- ruby/knightDragon/knightDragonA.rb
+++ ruby/knightDragon/knightDragonA.rb
@@ -1,25 +1,24 @@
-dragonHP = 50
-knightHP = 20
-
-def userPrompt (knightHP)
-    print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\', then press ENTER: "
-    userChoice = gets.upcase.chomp 
-    return userChoice 
-end
+$dragonHP = 10 #dragon starting hit points
+$knightHP = 4 #knight starting hit points
+dragonRoll = 0 #define dice roll parameter for dragon
+knightRoll = 0 #define dice roll parameter for knight
 
 def choiceCheck (userChoice)
     if userChoice == "FIGHT" 
         print "Initiate fighting!\n"
+        $knightHP = $knightHP - rand(1..6)
+        $dragonHP = $dragonHP - rand(1..6)
+        print "This round ended.\nKnight has " + $knightHP.to_s + " hit points remaining.\nDragon has " + $dragonHP.to_s + " hit points remaining.\n"
+        #knight rolls dice for damage to dragon; save results, report
+        #dragon rolls dice for damage to knight; save results, report
     elsif userChoice == "RUN"
         print "Knight successfully runs away.\n"
-        
     else 
         print "Your selection doesn't match any available options. Please try again.\n"
     end
 end 
 
-#print "Knight enters Dragon's lair with " + knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\', then press ENTER: "
-#userChoice = gets.upcase.chomp 
+print "Knight enters Dragon's lair with " + $knightHP.to_s + " HP. What would you like to do?\nType \'FIGHT\' or \'RUN\', then press ENTER: "
+userChoice = gets.upcase.chomp 
 
-userPrompt(knightHP) 
 choiceCheck(userChoice) 
