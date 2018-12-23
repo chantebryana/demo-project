2485b5a63c03599cf45fc34d83192fd39ad26a1a
chantebryana <chauncybryana@gmail.com>
2018-12-23 02:03:06 +0000

    little boxes on the hillside

--- ruby/moonphase/howdy.rb
+++ ruby/moonphase/howdy.rb
@@ -1,14 +1,13 @@
 require 'rubygems'
 require 'json'
 require 'date'
-=begin
-def jsonToArray (array) 
-    json = File.read('moonphase.json')
+
+def jsonToArray (jsonFile, arrayKey)
+    json = File.read(jsonFile)
     moonArray = JSON.parse(json)
-    array = moonArray["phasedata"]
-    return array 
+    return moonArray[arrayKey]
 end
-=end
+
 def phaseLength (x)
     a = []
     1.upto(x.length-1) do |i|
@@ -19,19 +18,7 @@ def phaseLength (x)
     return a
 end
 
+moonPhase = jsonToArray 'moonphase.json', "phasedata"
 
-#moonPhase = []
-#jsonToArray moonPhase
-json = File.read('moonphase.json')
-moonArray = JSON.parse(json)
-moonPhase = moonArray["phasedata"]
-
-#puts moonPhase[0]["date"]
-#dateRange = (Date.parse(moonPhase[8]["date"]) - Date.parse(moonPhase[4]["date"])).to_i
-#puts "#{dateRange} days"
-
-#puts moonPhase[0]["phase"]
-
-#puts moonPhase
 puts phaseLength moonPhase 
 
