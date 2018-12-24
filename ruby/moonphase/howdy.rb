d14b63b1439f23896d04c0e9752bd0c2baa1958c
chantebryana <chauncybryana@gmail.com>
2018-12-24 20:23:08 +0000

    fumble with dates: first steps to moonrise over lunar phase

--- ruby/moonphase/howdy.rb
+++ ruby/moonphase/howdy.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'json'
 require 'date'
 
@@ -20,5 +19,31 @@ end
 
 moonPhase = jsonToArray 'moonphase.json', "phasedata"
 
-puts phaseLength moonPhase 
+moonDays = phaseLength moonPhase 
+puts moonDays[1]
 
+#24 hours / 29 days = 0.8276 hours added to each day (while moon's set))
+
+#puts 24.to_f / moonDays[0]
+
+=begin
+#b = []
+0.upto(moonDays.length-1) do |i|
+    puts (24.to_f / moonDays[i])
+end
+#puts b
+=end
+
+#6:00am plus 10 minutes; then 29 days
+#puts Time.new(2019, 01, 06, 6, 0, 0, "-06:00") + 10*60
+#puts Time.new(2019, 01, 06, 6, 0, 0, "-06:00") + 29*60*60*24
+
+x = Time.new(2019, 01, 06, 6, 0, 0, "-06:00")
+#x = x + 49.655172*60
+puts x
+#=begin
+1.upto(29) do |i|
+    x = x + 49.655172*60 + 1*60*60*24
+    puts x
+end
+#=end
